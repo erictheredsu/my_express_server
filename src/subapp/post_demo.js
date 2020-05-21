c973f794c884c347d37893f3ebdf3818db5bc512
i070637 <eric.su@sap.com>
2020-05-21 17:12:45 +0800

    a login example

--- src/subapp/post_demo.js
+++ src/subapp/post_demo.js
@@ -7,31 +7,44 @@ let app = express();
 
 //handle request
 app.get('/helloworld', function(req, res){
-    res.send("hello world!");
-  })
+  res.send("hello world!");
+})
 
 //post www-form-urlencoded
 app.use(bodyParser.urlencoded({    
     extended: true
   }));
-  app.post('/urlencoded.svc', function(req,res){
-    res.send(req.body);
-  });
+app.post('/urlencoded.svc', function(req,res){
+  res.send(req.body);
+});
   
-  //post form-data
-  let multipart_app = multipart();
-  app.post('/form-data.svc', multipart_app, function(req, res){
-    res.send(req.body);
-  });
-  
-  //post application/json  similiar with text/plain of B1 SL
-  app.use(bodyParser.json({limit:'1mb'}));
-  app.post('/application-json.svc', function(req, res){
-    res.send(req.body);
-  })
+//post form-data
+let multipart_app = multipart();
+app.post('/form-data.svc', multipart_app, function(req, res){
+  res.send(req.body);
+});
 
-  module.exports = {
-      getSubApp : ()=>{
-          return app;
-      }
-  }
+//post application/json  similiar with text/plain of B1 SL
+app.use(bodyParser.json({limit:'1mb'}));
+app.post('/application-json.svc', function(req, res){
+  res.send(req.body);
+})
+
+//post login with www-form-urlencoded
+app.post('/login.svc', function(req,res){
+   let userInfo = req.body;
+   if(userInfo['uname'] === 'manager' && userInfo['psw'] === '1234'){
+      res.redirect('/success.html');
+    }
+    else{
+      res.send("your user name is unknown or your password is incorrect!");
+    }
+
+});
+
+
+module.exports = {
+    getSubApp : ()=>{
+        return app;
+    }
+}
